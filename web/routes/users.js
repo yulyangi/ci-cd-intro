60f8e000ba63e58c6878ba5bee3833e8d96780d1
yulyangi <julianglonti@outlook.com>
2024-08-30 21:55:54 +0300

    Update

--- web/routes/users.js
+++ web/routes/users.js
@@ -1,10 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-/* GET users listing. */
+/* GET users listing yes. */
 router.get('/users', function (req, res, next) {
   res.send('respond with a resource')
 })
-
 
 module.exports = router
